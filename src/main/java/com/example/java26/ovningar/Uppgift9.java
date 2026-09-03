5bf1091ef59ae0c1fb9464515820ec1e533d1e7f
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-03 13:06:50 +0200

    Uppgift 9 - metod sum med två int-parametrar som returnerar summan

---
+++ src/main/java/com/example/java26/ovningar/Uppgift9.java
@@ -0,0 +1,14 @@
+package com.example.java26.ovningar;
+
+public class Uppgift9 {
+    static void main() {
+
+        int result = sum(5, 10);
+        IO.println("Summan blir: " + result);
+
+    }
+    static int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+}
