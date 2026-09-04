414a9158b19156094b80ad132382ff6db9b85391
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-04 12:52:45 +0200

    Uppgift 15 - metod kallad makesTen som tar två parametrar. Om en av dessa är 10              eller summan av dessa blir 10 returnera true. Annars false.

---
+++ src/main/java/com/example/java26/ovningar/Uppgift15.java
@@ -0,0 +1,16 @@
+package com.example.java26.ovningar;
+
+public class Uppgift15 {
+    static void main() {
+
+        boolean result = makesTen(3, 4);
+        IO.println(result);
+    }
+
+    static boolean makesTen(int number1, int number2) {
+        return number1 == 10 || number2 == 10 || number1 + number2 == 10;
+    }
+
+
+
+}
