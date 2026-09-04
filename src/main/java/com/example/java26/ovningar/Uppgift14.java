63a2ae1adb92e6282d86c819d47612f485251eff
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-04 12:47:00 +0200

    Uppgift 14 - metod som skriver ut “Fizz” om inparametern är jämnt delbara med 3.              “Buzz” om jämnt delbara med 5 och “Fizz Buzz” om båda är sanna.

---
+++ src/main/java/com/example/java26/ovningar/Uppgift14.java
@@ -0,0 +1,26 @@
+package com.example.java26.ovningar;
+
+public class Uppgift14 {
+    static void main() {
+
+        fizzBuzz(15);
+
+
+    }
+
+
+    static void fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            IO.println("Fizz Buzz");
+        }
+        else if (number % 3 == 0) {
+            IO.println("Fizz");
+        }
+        else if (number % 5 == 0) {
+            IO.println("Buzz");
+        }
+
+
+    }
+}
+
