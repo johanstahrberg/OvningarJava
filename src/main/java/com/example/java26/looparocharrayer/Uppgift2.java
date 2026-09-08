7519d93d02b5841f1a215bc09aa47fb4da0fafb4
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-08 13:17:08 +0200

    Uppgift 2 - while-loop som halverar talet

---
+++ src/main/java/com/example/java26/looparocharrayer/Uppgift2.java
@@ -0,0 +1,14 @@
+package com.example.java26.looparocharrayer;
+
+public class Uppgift2 {
+    static void main() {
+        int number = 65536;
+
+        while (number > 2) {
+            IO.println(number);
+            number = number / 2;
+        }
+
+
+    }
+}
