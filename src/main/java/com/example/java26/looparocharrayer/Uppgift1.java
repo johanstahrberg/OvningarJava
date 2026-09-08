f667842b183e1a2ec0f5c22567b8a62722d69013
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-08 13:07:25 +0200

    Uppgift 1 - for-loop som skriver ut talen 1 till 16

---
+++ src/main/java/com/example/java26/looparocharrayer/Uppgift1.java
@@ -0,0 +1,12 @@
+package com.example.java26.looparocharrayer;
+
+public class Uppgift1 {
+    static void main() {
+
+        for (int i = 1; i <= 16; i++) {
+            IO.println(i);
+        }
+
+
+    }
+}
