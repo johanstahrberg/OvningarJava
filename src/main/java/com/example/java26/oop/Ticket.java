2be02fecb2cee7c5eda0b686267cc72dcb696f34
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-18 12:59:54 +0200

    Uppgift 2.3 - testar default constructor och constructor overloading

---
+++ src/main/java/com/example/java26/oop/Ticket.java
@@ -0,0 +1,13 @@
+package com.example.java26.oop;
+
+public class Ticket {
+
+    public Ticket() {
+    }
+
+    public Ticket(String eventName) {
+    }
+
+
+
+}
---
+++ src/main/java/com/example/java26/oop/Uppgift2_3.java
@@ -0,0 +1,12 @@
+package com.example.java26.oop;
+
+public class Uppgift2_3 {
+    static void main() {
+
+        Ticket ticket1 = new Ticket("Konsert");
+
+
+
+
+    }
+}
